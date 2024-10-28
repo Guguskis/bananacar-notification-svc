713fbad4f101fd2281771f8a6bae32e57055ed2a
Guguskis <matasliutikas@gmail.com>
2024-10-28 16:14:59 +0200

    init commit

---
+++ src/main/java/lt/liutikas/bananacar_notification_svc/BananacarNotificationSvcApplication.java
@@ -0,0 +1,13 @@
+package lt.liutikas.bananacar_notification_svc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BananacarNotificationSvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BananacarNotificationSvcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/lt/liutikas/bananacar_notification_svc/BananacarNotificationSvcApplicationTests.java
@@ -0,0 +1,13 @@
+package lt.liutikas.bananacar_notification_svc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BananacarNotificationSvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
