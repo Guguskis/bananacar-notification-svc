da2f7cb5ff7086808a6e5a27d7c9faedb7aff8d7
Guguskis <matasliutikas@gmail.com>
2024-11-09 19:53:50 +0200

    fixed ITestBase

--- src/test/java/lt/liutikas/bananacar_notification_svc/adapter/web/BananacarServiceITest.java
+++ src/test/java/lt/liutikas/bananacar_notification_svc/adapter/web/BananacarServiceITest.java
@@ -5,12 +5,10 @@ import lt.liutikas.bananacar_notification_svc.application.ScanRidesUseCase;
 import lt.liutikas.bananacar_notification_svc.domain.Ride;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.test.context.ActiveProfiles;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
-@ActiveProfiles({"itest", "secrets"})
 class BananacarServiceITest extends ITestBase {
 
     @Autowired
